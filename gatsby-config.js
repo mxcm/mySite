ba2f764cfc592696b76f48bfdfc0b1817bf60c00
Unknown <yehui.zh@gmail.com>
2019-06-22 07:57:39 -0700

    added remark plugin

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,10 +1,50 @@
 const config = require("./src/config/basics")
 
 module.exports = {
+  siteMetadata: {
+    title: config.siteTitle,
+    siteUrl: config.siteUrl,
+    description: config.siteDescription,
+  },
   plugins: [
     `gatsby-plugin-react-helmet`,
     `gatsby-plugin-robots-txt`,
     `gatsby-plugin-sitemap`,
+    `gatsby-plugin-sharp`,
+
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        name: `content`,
+        path: `${__dirname}/src/content/`,
+      },
+    },
+
+    {
+      resolve: `gatsby-transformer-remark`,
+      options: {
+        plugins: [
+          {
+            resolve: `gatsby-remark-images`,
+            options: {
+              // supports only JPG and PNG
+              maxWidth: 1000,
+              linkImagesToOriginal: true,
+              showCaptions: false,
+              quality: 60,
+            },
+          },
+          {
+            resolve: "gatsby-remark-external-links",
+            options: {
+              target: "_blank",
+              rel: "nofollow noopener noreferrer",
+            },
+          },
+        ],
+      },
+    },
+
     {
       resolve: `gatsby-plugin-google-analytics`,
       options: {
@@ -12,9 +52,4 @@ module.exports = {
       },
     },
   ],
-  siteMetadata: {
-    title: config.siteTitle,
-    siteUrl: config.siteUrl,
-    description: config.siteDescription,
-  },
 }
